91679781ef4da5dbb836944bd31dd4966bd051dc
Bayram208 <102304026+Bayram208@users.noreply.github.com>
2022-04-07 15:10:38 +0200

    bitti

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("privi")
+s1= input("gıve some")
+print(s1)
+
+islem=input("")
+print(s1,islem)
+
+s2=int(input(""))
+
+if islem=="+":
+    sonuc=(s1)+(s2)
+elif islem=="-":
+    sonuc=(s1)-(s2)
+elif islem=="*":
+    sonuc=(s1)*(s2)
+elif islem=="/":
+    sonuc=(s1)/(s2)
+else: print("hatalı işlem girdiniz")
+
+print('s1,islem,s2,"="sonuc')
